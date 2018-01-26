a9e64716fba7aeba48ec8e958dd7b3da6f3b206b
Rogério Takashi Hirata <rogeriotakashi@hotmail.com>
2018-01-26 15:27:15 -0200

    Created Saving.java and implemented computeSaving function

---
+++ app/src/main/java/rogerio/com/tsp/Optimize/Saving.java
@@ -0,0 +1,51 @@
+package rogerio.com.tsp.Optimize;
+
+/**
+ * Created by ROGERIO on 26/01/2018.
+ */
+
+public class Saving {
+
+    private int i;
+    private int j;
+    private double saving;
+
+    public Saving(int i, int j, double saving) {
+        this.i = i;
+        this.j = j;
+        this.saving = saving;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public double getSaving() {
+        return saving;
+    }
+
+    public void setSaving(double saving) {
+        this.saving = saving;
+    }
+
+    @Override
+    public String toString() {
+        return "Saving{" +
+                "i=" + i +
+                ", j=" + j +
+                ", saving=" + saving +
+                '}';
+    }
+}
--- app/src/main/java/rogerio/com/tsp/Optimize/SavingsHeuristic.java
+++ app/src/main/java/rogerio/com/tsp/Optimize/SavingsHeuristic.java
@@ -16,10 +16,12 @@ public class SavingsHeuristic {
 
     private ArrayList<Location> locations;
     private ArrayList<Route> savingsRoutes;
+    private ArrayList <Saving> savingList;
 
     public SavingsHeuristic(ArrayList<Location> locations) {
         this.locations = locations;
         this.savingsRoutes = new ArrayList<>();
+        this.savingList = new ArrayList<>();
     }
 
 
@@ -39,30 +41,54 @@ public class SavingsHeuristic {
 
     }
 
+    public void computeSavings(){
+        Location depot = locations.get(0);
 
-    public void optimize(){
+        for(int i = 1; i < locations.size();i++){
+            for(int j = i+1; j < locations.size();j++){
+                double saving = locations.get(i).distance(depot) + locations.get(j).distance(depot) - locations.get(i).distance(locations.get(j));
+                Saving savingObj = new Saving(i,j,saving);
+                savingList.add(savingObj);
+            }
+        }
 
-        createSavingRoutes();
 
     }
 
-    //Make n routes: v0 → vi → v0, for each i ≥ 1;
-
-    //Compute the savings for merging delivery locations i and j, which is given by sij = di0 + d0j − dij , for all i, j ≥ 1 and i 6= j;
-
-
-    //Sort the savings in descending order;
-
-    // Starting at the top of the (remaining) list of savings, merge the two routes associated with the largest (remaining) savings, provided that:
-    //(a) The two delivery locations are not already on the same route;
-    //(b) Neither delivery location is interior to its route, meaning that both notes are still
-    //directly connected to the depot on their respective routes;
-    //(c) The demand G and distance constraints D are not violated by the merged route.
+    public void printSavings(){
+        for(Saving saving : savingList){
+            Log.e("Saving in the list:",saving.toString());
+        }
+    }
 
 
+    public void optimize(){
+
+        //Make n routes: v0 → vi → v0, for each i ≥ 1;
+        createSavingRoutes();
+
+        //Compute the savings for merging delivery locations i and j, which is given by sij = di0 + d0j − dij , for all i, j ≥ 1 and i 6= j;
+        computeSavings();
+        printSavings();
+
+        //Sort the savings in descending order;
+
+        // Starting at the top of the (remaining) list of savings, merge the two routes associated with the largest (remaining) savings, provided that:
+        //(a) The two delivery locations are not already on the same route;
+        //(b) Neither delivery location is interior to its route, meaning that both notes are still
+        //directly connected to the depot on their respective routes;
+        //(c) The demand G and distance constraints D are not violated by the merged route.
 
 
-    //Repeat step (3) until no additional savings can be achieved.
+
+
+        //Repeat step (3) until no additional savings can be achieved.
+
+    }
+
+
+
+
 
 
 }
